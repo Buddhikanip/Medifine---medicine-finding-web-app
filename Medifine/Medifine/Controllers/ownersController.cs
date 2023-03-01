e4a9753739e8bd4b170c36b54d906cafbc6eb67f
Buddhika Nipun <bnsbuddhika@gmail.com>
2023-03-01 22:55:30 +0530

    Controller changed

---
+++ Medifine/Medifine/Controllers/ownersController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Medifine.Models;
+
+namespace Medifine.Controllers
+{
+    public class ownersController : Controller
+    {
+        private readonly MedifineContext _context;
+
+        public ownersController(MedifineContext context)
+        {
+            _context = context;
+        }
+
+        // GET: owners
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Owners.ToListAsync());
+        }
+
+        // GET: owners/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Owners == null)
+            {
+                return NotFound();
+            }
+
+            var owner = await _context.Owners
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            return View(owner);
+        }
+
+        // GET: owners/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: owners/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,PhoneNo,Email,Address,NIC")] owner owner)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(owner);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(owner);
+        }
+
+        // GET: owners/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Owners == null)
+            {
+                return NotFound();
+            }
+
+            var owner = await _context.Owners.FindAsync(id);
+            if (owner == null)
+            {
+                return NotFound();
+            }
+            return View(owner);
+        }
+
+        // POST: owners/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,PhoneNo,Email,Address,NIC")] owner owner)
+        {
+            if (id != owner.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(owner);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ownerExists(owner.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(owner);
+        }
+
+        // GET: owners/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Owners == null)
+            {
+                return NotFound();
+            }
+
+            var owner = await _context.Owners
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            return View(owner);
+        }
+
+        // POST: owners/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Owners == null)
+            {
+                return Problem("Entity set 'MedifineContext.Owners'  is null.");
+            }
+            var owner = await _context.Owners.FindAsync(id);
+            if (owner != null)
+            {
+                _context.Owners.Remove(owner);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ownerExists(int id)
+        {
+          return _context.Owners.Any(e => e.Id == id);
+        }
+    }
+}
